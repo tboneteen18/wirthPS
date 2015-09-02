fecf7dc9a4500c0e026b1ad3d7abc9eefff8c75f
tboneteen18 <tboneteen18@gmail.com>
2015-09-02 11:25:18 -0600

    fonts now loading and added the sticky on scroll to the menu

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+            $('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+          $('#nav').on('affix.bs.affix', function () {
+            var navHeight = $('.navbar').outerHeight(true);
+
+            $('#nav + .container').css('margin-top', navHeight);
+		});
+          $('#nav').on('affix-top.bs.affix', function () {
+             $('#nav + .container').css('margin-top', 0);
+		});
+
+
+$('#sidebar').affix({
+      offset: {
+        top: 17
+      }
+});
+
+        });
